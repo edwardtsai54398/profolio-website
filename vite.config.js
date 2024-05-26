cd4509d82943083c4e1fbe15da2bf56cf35ba681
LAPTOP-QB1B92GD\Edward <fashion.donk@gmail.com>
2024-05-26 11:57:00 +0800

    change baseURL

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/profolio-website/",
+  // base: "",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
